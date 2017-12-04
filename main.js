8cf338a34e10c4675ec95050887ed233643771eb
Milan Iliev <milaniliev@gmail.com>
2017-12-04 18:41:33 -0500

    Simple expression composition

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 class Expression {
-  constructor(pattern){
-    this.pattern = pattern
+  constructor(...components){
+    this.components = components
+  }
+  
+  get pattern(){
+    return this.components.map((component) => {
+      if(typeof component === 'string'){
+        return component
+      }
+    }).join('')
   }
 
   match(string){
-    let regexp = new RegExp(this.pattern)
-    let match_result = regexp.exec(string)
+    let regexp_result = new RegExp(this.pattern).exec(string)
 
-    return {
-      index: match_result.index
+    if(regexp_result){
+      return {
+        index: regexp_result.index,
+        found: true
+      }
+    } else {
+      return {
+        found: false
+      }
     }
   }
 }
--- spec/simple_match_spec.js
+++ spec/simple_match_spec.js
@@ -1,11 +1,33 @@
 let {expression} = require('../')
 
-describe("Simple matches", function() {
-  let simple_expression = expression("aaa")
+describe("Simple matches", () => {
+  let simple_expression = expression("aaa")    
+  
+  it("reports positive match", () => {
+    let match = simple_expression.match("baaa")
 
-  it("matches", function() {
+    expect(match.found).toBe(true)
+  })
+
+  it("reports negative match", () => {
+    let match = simple_expression.match("baq")
+
+    expect(match.found).toBe(false)
+  })
+
+  it("finds index of match", () => {
     let match = simple_expression.match("baaa")
 
     expect(match.index).toEqual(1)
+  })
+})
+
+describe("Simple composited expression", () => {
+  let composite_expression = expression("b", "aaa")
+
+  it("matches_correctly", () => {
+    let match = composite_expression.match("baaa")
+    expect(match.found).toBe(true)    
+    expect(match.index).toBe(0)
   })
 })
